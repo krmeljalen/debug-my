6833355b3e09ce3325b03d515e422acd8ce8bd6c
Alen Krmelj <krmelj.alen@gmail.com>
2023-08-21 01:33:32 +0200

    Added Memory performance

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import os
 import sys
+import time
 import psutil
 import multiprocessing
 
@@ -8,7 +9,16 @@ import multiprocessing
 # Threshold settings
 #
 
-cpu_usage_threshold_pct = 80
+# CPU thresholds
+cpu_usage_threshold_pct = 80  # Percent
+cpu_iowait_threshold_pct = 10  # Percent
+cpu_steal_threshold_pct = 1  # Percent
+cpu_ctx_switches_threshold = 5000  # Per second
+cpu_interrupts_threshold = 2000  # Per second
+
+# Memory thresholds
+memory_available_threshold = 20  # Percent
+swap_usage_threshold = 0  # Bytes
 
 #
 # Stdout helpers
@@ -54,12 +64,19 @@ def get_cpu_performance():
     cpu_data["cpu_times"] = psutil.cpu_times_percent()
     # scpustats(ctx_switches=26570269, interrupts=17106030, soft_interrupts=8205427, syscalls=0)
     cpu_data["cpu_stats"] = psutil.cpu_stats()
-    # scpufreq(current=0.649, min=500.0, max=2700.0)
-    cpu_data["cpu_freq"] = psutil.cpu_freq()
     return cpu_data
 
 
 # Memory helpers
+
+
+def get_memory_stats():
+    # svmem(total=8195940352, available=5540249600, percent=32.4, used=2006110208, free=1421676544, active=2616713216, inactive=3336273920, buffers=611999744, cached=4156153856, shared=352309248, slab=439496704)
+    memory_data = {}
+    memory_data["memory_usage"] = psutil.virtual_memory()
+    memory_data["swap_usage"] = psutil.swap_memory()
+    return memory_data
+
 
 #
 # Report functions
@@ -73,41 +90,102 @@ def report_server_load():
     for system_load in system_loads[:3]:
         if float(system_load) > cpu_count:
             warning(
-                "Server load too high! ({}) which is higher than {} cpu cores available.".format(
+                "Server load too high! ({} > {})".format(
                     ", ".join(system_loads[:3]), cpu_count
                 )
             )
             return
 
     # Default response
-    info("Server load is fine. Below {} cpu cores available.".format(cpu_count))
+    info("Server load is fine, below {} cpu cores.".format(cpu_count))
 
 
 def report_cpu_usage():
     cpu_usage = get_cpu_usage()
     if cpu_usage > cpu_usage_threshold_pct:
         warning(
-            "Server CPU usage is too high! Current cpu usage is {}% which is > {}% ".format(
+            "Server CPU usage is too high! ({}% > {}%)".format(
                 round(cpu_usage), cpu_usage_threshold_pct
             )
         )
         return
 
     info(
-        "Server CPU usage looks fine. Current CPU usage is {}% which is < {}%.".format(
+        "Server CPU usage looks fine. ({}% < {}%)".format(
             round(cpu_usage), cpu_usage_threshold_pct
         )
     )
 
 
 def report_cpu_performance():
+    cpu_performance_stats = get_cpu_performance()
     # IO Wait
+    if cpu_performance_stats["cpu_times"].iowait > cpu_iowait_threshold_pct:
+        warning(
+            "IO wait is high! ({}% > {}%) This means you are doing a lot of network or disk operations that CPU is waiting to finish.".format(
+                cpu_performance_stats["cpu_times"].iowait, cpu_iowait_threshold_pct
+            )
+        )
     # Steal time
+    if cpu_performance_stats["cpu_times"].steal > 1:
+        warning(
+            "CPU Steal detected! ({}% > {}%) Other VMs on particular host are affecting this server CPU resources. Try moving VM to another host that is not overloaded.".format(
+                cpu_performance_stats["cpu_times"].steal, cpu_steal_threshold_pct
+            )
+        )
+    # Per second stats
+    stats_1 = get_cpu_performance()
+    time.sleep(1)
+    stats_2 = get_cpu_performance()
+
     # Context switches
+    ctx_switches_per_second = (
+        stats_2["cpu_stats"].ctx_switches - stats_1["cpu_stats"].ctx_switches
+    )
+    if ctx_switches_per_second > cpu_ctx_switches_threshold:
+        warning(
+            "CPU is doing high amount of context switching. (~{}/s > {}/s) Try pinning running processes to specific cores or spearate part of application logic to another server.".format(
+                ctx_switches_per_second, cpu_ctx_switches_threshold
+            )
+        )
+
     # Interrupts
-    # Frequencies
+    interrupts_per_second = (
+        stats_2["cpu_stats"].interrupts - stats_1["cpu_stats"].interrupts
+    )
+    if interrupts_per_second > cpu_interrupts_threshold:
+        warning(
+            "CPU is being often interrupted by hardware events. (~{}/s > {}/s)".format(
+                interrupts_per_second, cpu_interrupts_threshold
+            )
+        )
+
     # Cache hit ratio
+    # Cannot be retrieved without `perf` tool
     return
+
+
+def report_memory_usage():
+    memory_stats = get_memory_stats()
+    available_mem_pct = round(
+        memory_stats["memory_usage"].available
+        / memory_stats["memory_usage"].total
+        * 100
+    )
+    if available_mem_pct < memory_available_threshold:
+        warning(
+            "Available memory is low! ({}% < {}%)".format(
+                available_mem_pct, memory_available_threshold
+            )
+        )
+    # Swap usage
+    if memory_stats["swap_usage"].used > swap_usage_threshold:
+        warning(
+            "Server was swapping at one point! ({} > {}) Consider adding more memory to the server.".format(
+                psutil._common.bytes2human(memory_stats["swap_usage"].used),
+                swap_usage_threshold,
+            )
+        )
 
 
 def main():
@@ -117,7 +195,7 @@ def main():
     report_cpu_performance()
 
     header("MEMORY")
-    # Memory
+    report_memory_usage()
 
     # Disk
 
